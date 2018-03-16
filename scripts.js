fa07de40b28e20d8c8d0a103391d955108da8962
Pavel Trekell <pavel.trekell@expeditors.com>
2018-03-16 08:20:49 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function loadButtons(){
+    let div = document.getElementById('template-buttons');
+    div.style.visibility = 'visible';
+  }
+
+  // produces Excel template based on model
+  function downloadTemplate(){
+    let selectedTemplate = document.querySelector('input[name="radio-1"]:checked').id;
+    console.log(selectedTemplate);
+  }
+
+   // upload template
+  function uploadTemplate(){
+    console.log("uploading...")
+  } 
