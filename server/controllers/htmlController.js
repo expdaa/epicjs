992eef8d719c70687883c89b24928ec09bb29c0a
Pavel Trekell <pavel.trekell@expeditors.com>
2018-03-16 13:29:43 -0700

    still building folder structure

---
+++ server/controllers/htmlController.js
@@ -0,0 +1,8 @@
+module.exports = function (app,path,dirname) {
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(dirname + '/client/index.html'));//main index file
+    });
+
+
+}
---
+++ serverApp.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const htmlController = require('./server/controllers/htmlController');
+
+
+// app.use('/favicon.ico', express.static(path.join(__dirname,'/public/img/favicon.ico'))); //make favicon available
+app.use('/public', express.static(path.join(__dirname, 'client')))
+
+htmlController(app,path,__dirname);
+
+
+app.listen(3000, () => console.log('Listening on port 3000!')) //listen on port 3000
