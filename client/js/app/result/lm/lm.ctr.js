4cc9bd9facc90d3c3223b12fb849f59994342a28
Pavel Trekell <pavel.trekell@expeditors.com>
2018-03-20 14:09:30 -0700

    merging cleanup

--- client/js/app/result/lm/lm.ctr.js
+++ client/js/app/result/lm/lm.ctr.js
@@ -48,22 +48,7 @@
         };
 
 
-        $scope.plData = [trace1, trace2];
-var trace1 = {
-  x: plotX,
-  y: plotY,
-  name: 'Data',
-  mode: 'markers'
-};
-
-var trace2 = {
-  x: lineX,
-  y: lineY,
-  name: 'Regression',
-  mode: 'lines'
-  
-};
-
+ 
 
         $scope.plData = [ trace1, trace2];
         $scope.plLayout = {};
