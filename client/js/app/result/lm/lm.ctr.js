483cb7b4eb585173622a09d4560737e1bfc96b7e
Pavel Trekell <pavel.trekell@expeditors.com>
2018-03-20 07:03:35 -0700

    more work done on chart.js

--- client/js/app/result/lm/lm.ctr.js
+++ client/js/app/result/lm/lm.ctr.js
@@ -3,36 +3,70 @@
     angular
         .module("epicJS")
         .controller("LMController", LMController)
-        .config(['ChartJsProvider', function (ChartJsProvider) {
-            // Configure all charts
-            ChartJsProvider.setOptions({
-                chartColors: ['#FF5252', '#d0d0d0'],
-                responsive: false
-            });
-            // Configure all line charts
-            ChartJsProvider.setOptions('line', {
-                showLines: true
-            });
-        }]);
-
-    LMController.$inject = ['lmResource'];
 
 
-    function LMController(lmResource) {
+    LMController.$inject = ['lmResource', '$scope'];
+
+
+    function LMController(lmResource, $scope) {
         var vm = this;
 
         vm.lmResource = lmResource;
 
-
-        vm.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        vm.series = ['Series A', 'Series B'];
-        vm.data = [
+        $scope.first_labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.first_series = ['Series A', 'Series B'];
+        $scope.first_data = [
             [65, 59, 80, 81, 56, 55, 40],
             [28, 48, 40, 19, 86, 27, 90]
         ];
-        vm.onClick = function (points, evt) {
+        $scope.onClick = function (points, evt) {
             console.log(points, evt);
         };
+
+      
+
+        $scope.first_datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        $scope.first_options = {
+          
+            elements: {
+                line:
+                    {
+                        fill: false
+                    }
+            },
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left',
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)",
+                        }
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right',
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)",
+                        }
+                    }
+                ],
+
+                //https://stackoverflow.com/questions/36676263/chart-js-v2-hiding-grid-lines
+                xAxes: [
+                    {
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)",
+                        }   
+                    }
+                ]
+            }
+        };
+
 
 
 
